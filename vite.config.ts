f145fcd0bbd8b17d312064b13d81c53465533b7a
riodio1911-design <riodio1911@gmail.com>
2026-04-19 19:18:05 +0700

    fix loi process sap web

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import {defineConfig, loadEnv} from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: './',
+	define: {
+		'process.env.GEMINI_API_KEY:JSON.stringify(env.VITE_GEMINI_API_KEY)
+		}
     plugins: [
       react(), 
       tailwindcss(),
